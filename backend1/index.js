8d8e5187372a95b39103a3c17db0d8deea5d33fd
Jebajuan <jebaimmanuel@gmail.com>
2025-02-25 13:19:52 +0530

    Day 13 FN

--- backend1/index.js
+++ backend1/index.js
@@ -1,11 +1,41 @@
-const express = require('express')
+const express = require("express");
+const mdb = require("mongoose");
+const dotenv = require("dotenv");
+const Signup = require("./models/signupSchema");
+const app = express();
+app.use(express.json())
+const PORT = 3001;
+dotenv.config();
 
-const app = express()
+mdb
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("Connection Successfully");
+  })
+  .catch((err) => {
+    console.log("Check your connection string", err);
+  });
 
-app.get("/",(req,res)=>{
-    res.send("<h1> Welcome to Backend Server</h1>")
-})
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to server</h1>");
+});
 
-const PORT = 3001
+app.post("/signup", (req, res) => {
+  try {
+    const {firstName,lastName,password,email,phoneNumber}=req.body
+    const newSignup = new Signup({
+      firstName: firstName,
+      lastName: lastName,
+      password: password,
+      email: email,
+      phoneNumber: phoneNumber,
+    });
+    newSignup.save();
+    console.log("Signup Successful");
+    res.status(201).json({ message: "Signup Successful", isSignup: true });
+  } catch (error) {
+    res.status(201).json({ message: "Signup Failed", isSignup: false });
+  }
+});
 
-app.listen(PORT,()=>console.log("Server started successfully"))
+app.listen(PORT, () => console.log("Server started successfully"));
