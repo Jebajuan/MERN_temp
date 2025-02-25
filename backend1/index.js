9a023bf3ce0ca3843d4aade47558c5eeb63d9808
Jebajuan <jebaimmanuel@gmail.com>
2025-02-25 21:18:14 +0530

    Day 13 task

--- backend1/index.js
+++ backend1/index.js
@@ -3,6 +3,7 @@ const mdb = require("mongoose");
 const dotenv = require("dotenv");
 const Signup = require("./models/signupSchema");
 const app = express();
+const bcryt=require('bcrypt')
 app.use(express.json())
 const PORT = 3001;
 dotenv.config();
@@ -20,13 +21,34 @@ app.get("/", (req, res) => {
   res.send("<h1>Welcome to server</h1>");
 });
 
-app.post("/signup", (req, res) => {
+app.post("/login",async (req,res)=>{
+  const{Email, password} = req.body;
+  try {
+    const email=await Signup.findOne({Email})
+    if(!email){
+      return res.status(400).json({message:"Invalid email"})
+    }
+
+    const isMatch=await bcryt.compare(password,email.password)
+    if(!isMatch){
+      return res.status(400).json({message:"Invalid email or password"})
+    }
+
+    return res.status(200).json({message:"Login Successful"})
+
+  } catch (error) {
+    res.status(400).json({message:"Login Failed"})
+  }
+})
+
+app.post("/signup", async (req, res) => {
   try {
     const {firstName,lastName,password,email,phoneNumber}=req.body
+    const hashPassword=await bcryt.hash(password,10);
     const newSignup = new Signup({
       firstName: firstName,
       lastName: lastName,
-      password: password,
+      password: hashPassword,
       email: email,
       phoneNumber: phoneNumber,
     });
@@ -34,7 +56,7 @@ app.post("/signup", (req, res) => {
     console.log("Signup Successful");
     res.status(201).json({ message: "Signup Successful", isSignup: true });
   } catch (error) {
-    res.status(201).json({ message: "Signup Failed", isSignup: false });
+    res.status(400).json({ message: "Signup Failed", isSignup: false });
   }
 });
 
