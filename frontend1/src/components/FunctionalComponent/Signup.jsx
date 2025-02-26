599b6a05da10a779f9de03839b01b5cabdeb018b
Jebajuan <jebaimmanuel@gmail.com>
2025-02-26 13:27:47 +0530

    Day 14 AN

--- frontend1/src/components/FunctionalComponent/Signup.jsx
+++ frontend1/src/components/FunctionalComponent/Signup.jsx
@@ -1,28 +1,53 @@
-import { useState,useEffect } from "react"
+import { useState } from "react"
 import { Link } from "react-router-dom"
+import {useNavigate} from "react-router-dom"
+import axios from 'axios';
 
 function Signup() {
-  var [name,setName]=useState("")
-  var [email,setEmail]=useState("")
-  var [pass,setPass]=useState("")
-  var [cpass,setCpass]=useState("")
+  const navigate =useNavigate();
+  const [firstName,setFN]=useState("");
+ const [lastName,setLN]=useState("");
+ const [email,setEmail]=useState("");
+ const [password,setPassword]=useState("");
+ const [phoneNumber,setPhone]=useState(0);
+ const handleSignup=async (event)=>{
+  event.preventDefault()
+  const req= await axios.post("http://localhost:3001/signup",{
+    firstName:firstName,
+    lastName:lastName,
+    email:email,
+    password:password,
+    phoneNumber:phoneNumber
+  })
+  const message =req.data.message
+  const isSignup=req.data.isSignup
+  if(isSignup){
+    alert(message)
+    navigate("/login")
+  }else{
+    alert(message)
+  }
+  
+ }
   return (
     <div>
       <h1>Signup Page</h1>
-      <form action="">
-          UserName: <input type="text" placeholder="Name" onChange={(e)=>setName(e.target.value)} />
-          UserName: <input type="text" id="uname" placeholder="Name" onChange={(e)=>setName(e.target.value)} required/>
-          <h3>The Name is: {name} </h3>
-          Email: <input type="email" placeholder="Email" onChange={(e)=>setEmail(e.target.value)} />
-          Email: <input type="email" id="email" placeholder="Email" onChange={(e)=>setEmail(e.target.value)} required/>
-          <h3>The Email is: {name} </h3>
-          Password: <input type="password" placeholder="password" onChange={(e)=>setPass(e.target.value)} />
-          Password: <input type="password" id="password" placeholder="password" onChange={(e)=>setPass(e.target.value)} required/>
+      <form onSubmit={handleSignup}>
+          <label htmlFor="firstName">FirstName:</label> 
+          <input type="text" id="firstName" value={firstName} onChange={(e)=> setFN (e.target.value)} required />
           <br />
-          Confirm Password: <input type="password" placeholder="confirm password" onChange={(e)=>setCpass(e.target.value)} />
-          Confirm Password: <input type="password" placeholder="confirm password" onChange={(e)=>setCpass(e.target.value)} required/>
+          <label htmlFor="lastName">LastName:</label>
+          <input type="text" id="lastName" value={lastName} onChange={(e)=> setLN (e.target.value)} required />
           <br />
-          <button onClick={()=>setCpass()}>SignUp</button>
+          <label htmlFor="email">Email:</label>
+          <input type="text" id="email" value={email} onChange={e=> setEmail ((e).target.value)} required />
+          <br />
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" value={password} onChange={(e)=> setPassword (e.target.value)} required />
+          <br />
+          <label htmlFor="phoneNumber">Phone Number:</label>
+          <input type="number" id="phoneNumber" value={phoneNumber} onChange={(e)=> setPhone (e.target.value)} required />
+          <br />
           <button type="submit">SignUp</button>
       </form>
       <p>Already have an account? <Link to='/login'>Login</Link> </p>
